1a56179f86e0be317738e0b0749be4a2d82a8406
Patrick Damme <patrick.damme@tu-dresden.de>
2019-06-24 18:51:51 +0200

    First basic version of a tool converting MorphStore's monitoring CSV files to JSON files for the web interface.

---
+++ tools/csv2json/csv2json.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+
+#*********************************************************************************************
+# Copyright (C) 2019 by MorphStore-Team                                                      *
+#                                                                                            *
+# This file is part of MorphStore - a compression aware vectorized column store.             *
+#                                                                                            *
+# This program is free software: you can redistribute it and/or modify it under the          *
+# terms of the GNU General Public License as published by the Free Software Foundation,      *
+# either version 3 of the License, or (at your option) any later version.                    *
+#                                                                                            *
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  *
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *
+# See the GNU General Public License for more details.                                       *
+#                                                                                            *
+# You should have received a copy of the GNU General Public License along with this program. *
+# If not, see <http://www.gnu.org/licenses/>.                                                *
+#*********************************************************************************************
+
+"""
+This tool converts CSV files output by MorphStore's monitoring feature to JSON
+files suitable for use in the web interface.
+
+Two kinds of CSV files can be converted, both of which can be created using
+"ssb.sh".
+- Operator (runtime) measurements: CSV files in the directory "time_sfN"
+  created by "ssb.sh" with "-p t".
+- Data measurements: CSV files in the directory "dc_sfN" created by "ssb.sh"
+  with "-p d".
+Where N is the scale factor.
+
+Known limitations:
+- Some of the data characteristics are filled with dummy data, since, at the
+  moment, they are not recorded in MorphStore.
+"""
+
+
+import argparse
+import csv
+import json
+import random
+    
+
+# *****************************************************************************
+# Constants
+# *****************************************************************************
+
+MODE_OPS = "ops"
+MODE_DATA = "data"
+MODES = [MODE_OPS, MODE_DATA]
+
+
+# *****************************************************************************
+# Translation functions
+# *****************************************************************************
+
+def convertOperatorFile(inCsvFile):
+    ATTR_OPNAME = "opName"
+    ATTR_OPIDX = "opIdx"
+    ATTR_RUNTIME = "runtime"
+    
+    reader = csv.DictReader(
+            inCsvFile,
+            fieldnames=[ATTR_OPNAME, ATTR_OPIDX, ATTR_RUNTIME],
+            delimiter="\t"
+    )
+    
+    # Skip the useless lines at the beginning.
+    for i in range(4):
+        next(reader)
+
+    res = {}
+    for row in reader:
+        # Skip the query results.
+        if row[ATTR_OPNAME] == "[RES]":
+            break
+            
+        key = row[ATTR_OPNAME] \
+                if row[ATTR_OPNAME] == "query" \
+                else "{}_{}".format(row[ATTR_OPNAME], int(row[ATTR_OPIDX]) - 1)
+        res[key] = {
+            ATTR_RUNTIME: int(row[ATTR_RUNTIME])
+        }
+        
+    return res
+
+def convertDataChFile(inCsvFile):
+    ATTR_OPNAME = "opName"
+    ATTR_COLNAME = "colName"
+    ATTR_VALUECOUNT = "valueCount"
+    ATTR_SIZEUSEDBYTE = "sizeUsedByte"
+    ATTR_FORMAT = "formatName"
+    ATTR_BWHIST_FS = "bwHist_{}"
+    ATTR_SORTED = "sorted"
+    ATTR_UNIQUE = "unique"
+    
+    reader = csv.DictReader(
+            inCsvFile,
+            fieldnames=[
+                ATTR_OPNAME,
+                "opIdx",
+                "colRole",
+                ATTR_COLNAME,
+                ATTR_VALUECOUNT,
+                "isResult"
+            ],
+            delimiter="\t"
+    )
+    
+    # Skip the useless lines at the beginning.
+    for i in range(4):
+        next(reader)
+
+    # For the dummy data.
+    formats = ["uncompr_f", "static_vbp_f", "dynamic_vbp_f", "rle"]
+    
+    res = {}
+    for row in reader:
+        if row[ATTR_OPNAME] == "[RES]":
+            break
+        res[row[ATTR_COLNAME]] = {
+            ATTR_VALUECOUNT: int(row[ATTR_VALUECOUNT]),
+            # Dummy data
+            ATTR_SIZEUSEDBYTE: random.randint(10**2, 10**8),
+            ATTR_FORMAT : formats[random.randrange(len(formats))],
+            ATTR_SORTED : [False, True][random.randrange(2)],
+            ATTR_UNIQUE : [False, True][random.randrange(2)],
+#            # Real data
+#            ATTR_PHYSIZE: int(row[ATTR_PHYSIZE]),
+#            ATTR_FORMAT : row[ATTR_FORMAT],
+#            ATTR_SORTED : bool(row[ATTR_FORMAT]),
+#            ATTR_UNIQUE : bool(row[ATTR_FORMAT]),
+        }
+        for bw in range(1, 64+1):
+            key = ATTR_BWHIST_FS.format(bw)
+            # Dummy data
+            res[row[ATTR_COLNAME]][key] = int(res[row[ATTR_COLNAME]][ATTR_VALUECOUNT] / 64)
+#            # Real data
+#            res[row[ATTR_COLNAME]][key] = int(row[ATTR_COLNAME][key])
+        
+    return res
+
+
+# *****************************************************************************
+# Main program
+# *****************************************************************************
+
+if __name__ == "__main__":
+    # -------------------------------------------------------------------------
+    # Parsing the command line arguments
+    # -------------------------------------------------------------------------
+    
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    # Required (positional) arguments.
+    parser.add_argument(
+        "mode", metavar="MODE", choices=MODES,
+        help="The kind of CSV file to convert, the following are supported: "
+            "{}.".format(", ".join(MODES))
+    )
+    # Optional arguments
+    FROM_STDIN = "-"
+    parser.add_argument(
+        "--csvfile", dest="inCsvFilePath", default=FROM_STDIN, metavar="FILE",
+        help="The path to the input CSV file. If this argument is omitted or "
+            "if '{}' is specified, then the data will be read from "
+            "stdin.".format(FROM_STDIN)
+        # TODO Validate existence.
+    )
+
+    args = parser.parse_args()
+
+    if args.inCsvFilePath == FROM_STDIN:
+        # 0 is the file descriptor of stdin, can be used with open().
+        args.inCsvFilePath = 0
+        
+    # -------------------------------------------------------------------------
+    # Translating CSV to JSON
+    # -------------------------------------------------------------------------
+
+    with open(args.inCsvFilePath, "r", newline="") as inCsvFile:
+        if args.mode == MODE_OPS:
+            res = convertOperatorFile(inCsvFile)
+        elif args.mode == MODE_DATA:
+            res = convertDataChFile(inCsvFile)
+        else:
+            raise RuntimeError("unsupported mode: '{}'".format(args.mode))
+
+    print(json.dumps(res, indent=2, sort_keys=True))
---
+++ tools/csv2json/csv2json.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for extension in $1
+do
+    for comprBase in $2
+    do
+        for variant in $3
+        do
+            key=$extension"_"$comprBase"_"$variant
+            mkdir --parents $key
+            for major in 1 2 3 4
+            do
+                for minor in 1 2 3
+                do
+                    ./csv2json.py --csvfile ../../ssb/time_sf1_$key/q$major.$minor.csv ops \
+                        > $key/ops_q$major.$minor.json
+                    ./csv2json.py --csvfile ../../ssb/dc_sf1_$key/q$major.$minor.csv data \
+                        > $key/data_q$major.$minor.json
+                done
+            done
+        done
+    done
+done
